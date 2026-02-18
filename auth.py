c959d4f8d63cf318861b7d7204161284c8464e15
Nehemiah Daniel <danielnehemiah15@gmail.com>
2026-02-19 00:48:24 +0100

    Add authentication

--- auth.py
+++ auth.py
@@ -7,13 +7,20 @@ from database import get_db
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from utils import authenticate_user, create_access_token, get_password_hash
-from schemas import UserCreate, UserResponse
+from utils import (
+    authenticate_user,
+    authenticate_vendor,
+    create_access_token,
+    get_password_hash,
+    get_current_active_user,
+    get_current_active_vendor,
+)
+from schemas import UserCreate, UserResponse, VendorCreate, VendorResponse
 
 router = APIRouter()
 
 
-@router.post("/token")
+@router.post("/token/user")
 async def login_user(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db: Annotated[AsyncSession, Depends(get_db)],
@@ -29,16 +36,32 @@ async def login_user(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@router.post("/signup", response_model=UserResponse)
-async def signup(user: UserCreate, db: Annotated[AsyncSession, Depends(get_db)]):
+@router.post("/token/vendor")
+async def login_vendor(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Annotated[AsyncSession, Depends(get_db)],
+) -> dict:
+    vendor = await authenticate_vendor(form_data.username, form_data.password, db)
+    if not vendor:
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub": vendor.email})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/signup/user", response_model=UserResponse)
+async def user_signup(user: UserCreate, db: Annotated[AsyncSession, Depends(get_db)]):
     result = await db.execute(
-        select(models.Users).where(models.Users.username == user.username)
+        select(models.User).where(models.User.username == user.username)
     )
     db_user = result.scalars().first()
     if db_user:
         raise HTTPException(status.HTTP_409_CONFLICT, detail="Username already exist")
     result = await db.execute(
-        select(models.Users).where(models.Users.email == user.email)
+        select(models.User).where(models.User.email == user.email)
     )
     user_email = result.scalars().first()
     if user_email:
@@ -50,14 +73,64 @@ async def signup(user: UserCreate, db: Annotated[AsyncSession, Depends(get_db)])
             status.HTTP_400_BAD_REQUEST, detail="Password must be atleast 8 characters"
         )
 
-    new_user = models.Users(
+    new_user = models.User(
         fullname=user.fullname,
         username=user.username,
         email=user.email,
-        password=password_hash,
+        password_hash=password_hash,
     )
 
     db.add(new_user)
     await db.commit()
     await db.refresh(new_user)
     return new_user
+
+
+@router.post("/signup/vendor", response_model=VendorResponse)
+async def vendor_signup(
+    vendor: VendorCreate, db: Annotated[AsyncSession, Depends(get_db)]
+):
+    result = await db.execute(
+        select(models.Vendor).where(models.Vendor.store_name == vendor.store_name)
+    )
+    db_vendor = result.scalars().first()
+    if db_vendor:
+        raise HTTPException(status.HTTP_409_CONFLICT, detail="Store Name already exist")
+    result = await db.execute(
+        select(models.Vendor).where(models.Vendor.email == vendor.email)
+    )
+    vendor_email = result.scalars().first()
+    if vendor_email:
+        raise HTTPException(status.HTTP_409_CONFLICT, detail="Email already exist")
+    if len(vendor.password) >= 8:
+        password_hash = get_password_hash(vendor.password)
+    else:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST, detail="Password must be atleast 8 characters"
+        )
+
+    new_vendor = models.Vendor(
+        fullname=vendor.fullname,
+        store_name=vendor.store_name,
+        email=vendor.email,
+        password_hash=password_hash,
+    )
+
+    db.add(new_vendor)
+    await db.commit()
+    await db.refresh(new_vendor)
+    return new_vendor
+
+
+@router.get("/me")
+async def read_users_me(
+    current_user: Annotated[UserResponse, Depends(get_current_active_user)],
+) -> UserResponse:
+    return current_user
+
+
+@router.get("/vendor")
+async def read_vendor(
+    current_user: Annotated[VendorResponse, Depends(get_current_active_vendor)],
+) -> VendorResponse:
+    return current_user
