e886781a646a36c3e27a37ef0250df17b9555e21
Nehemiah Daniel <danielnehemiah15@gmail.com>
2026-03-29 13:51:48 +0100

    update settings

--- src/app/config.py
+++ src/app/config.py
@@ -11,14 +11,16 @@ class Settings(BaseSettings):
         case_sensitive=False,
     )
 
-    sqlalchemy_database_url: str | None = None
+    sqlalchemy_database_url: str
     paystack_secret_key: SecretStr
-    paystack_url: str | None = None
-    jwt_secret_key: str | None = None
+    paystack_url: str
+    jwt_secret_key: str
     hashing_algorithm: str = "HS256"
     access_token_expire_minutes: int = 60
-    refresh_token_secret: str | None = None
+    refresh_token_secret: str
     refresh_token_expire_days: int = 30
+    mail_username: str
+    mail_password: SecretStr
 
 
 settings = Settings()
