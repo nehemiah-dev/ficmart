477924845c426377582703c18499e3b50dee13fa
Nehemiah Daniel <danielnehemiah15@gmail.com>
2026-03-29 13:47:34 +0100

    Feat: Add password reset

--- src/app/routes/auth.py
+++ src/app/routes/auth.py
@@ -1,10 +1,11 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status
+from datetime import datetime, timedelta, UTC
+from fastapi import APIRouter, Depends, HTTPException, status, Form
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.models import models
 from app.database import get_db
-from sqlalchemy import select
+from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.utils.auth import (
@@ -16,13 +17,18 @@ from app.utils.auth import (
     get_current_active_user,
     get_current_active_vendor,
 )
+from app.utils.tokens import generate_reset_token
 from app.schemas.user import UserCreate, UserResponse
 from app.schemas.vendor import VendorCreate, VendorResponse
+from app.schemas.password_reset import PasswordResetCreate
+from app.services.password_reset import save_reset_details, update_password
+
+from app.workers.mail_workers import send_signup_mail, send_reset_email
 
 router = APIRouter()
 
 
-@router.post("/user")
+@router.post("/login/user")
 async def login_user(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db: Annotated[AsyncSession, Depends(get_db)],
@@ -43,7 +49,7 @@ async def login_user(
     }
 
 
-@router.post("/vendor")
+@router.post("/login/vendor")
 async def login_vendor(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db: Annotated[AsyncSession, Depends(get_db)],
@@ -64,7 +70,7 @@ async def login_vendor(
     }
 
 
-@router.post("/signup/user", response_model=UserResponse)
+@router.post("/signup/user", response_model=UserResponse, status_code=201)
 async def user_signup(user: UserCreate, db: Annotated[AsyncSession, Depends(get_db)]):
     result = await db.execute(
         select(models.User).where(models.User.username == user.username)
@@ -77,13 +83,13 @@ async def user_signup(user: UserCreate, db: Annotated[AsyncSession, Depends(get_
     )
     user_email = result.scalars().first()
     if user_email:
-        raise HTTPException(status.HTTP_409_CONFLICT, detail="Username already exist")
-    if len(user.password) >= 8:
-        password_hash = get_password_hash(user.password)
-    else:
+        raise HTTPException(status.HTTP_409_CONFLICT, detail="Email already exist")
+    if len(user.password) < 8:
         raise HTTPException(
             status.HTTP_400_BAD_REQUEST, detail="Password must be atleast 8 characters"
         )
+    else:
+        password_hash = get_password_hash(user.password)
 
     new_user = models.User(
         fullname=user.fullname,
@@ -95,10 +101,12 @@ async def user_signup(user: UserCreate, db: Annotated[AsyncSession, Depends(get_
     db.add(new_user)
     await db.commit()
     await db.refresh(new_user)
+
+    send_signup_mail.delay(user.email)
     return new_user
 
 
-@router.post("/signup/vendor", response_model=VendorResponse)
+@router.post("/signup/vendor", response_model=VendorResponse, status_code=201)
 async def vendor_signup(
     vendor: VendorCreate, db: Annotated[AsyncSession, Depends(get_db)]
 ):
@@ -146,3 +154,63 @@ async def read_vendor(
     current_user: Annotated[VendorResponse, Depends(get_current_active_vendor)],
 ) -> VendorResponse:
     return current_user
+
+
+@router.post("/forgot-password")
+async def forgot_password(email: str, db: Annotated[AsyncSession, Depends(get_db)]):
+    if not email:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST, detail="Please provide your email"
+        )
+    result = await db.execute(select(models.User).where(models.User.email == email))
+    user = result.scalars().first()
+    if not user:
+        return {}
+    try:
+        token = generate_reset_token()
+    except RuntimeError as e:
+        return {"message": f"Could not generate token {e}"}
+    delta = timedelta(minutes=15)
+    now = datetime.now(UTC)
+    expires = now + delta
+    data = PasswordResetCreate(
+        token=token, user_id=user.id, used=False, expires_at=expires
+    )
+    await save_reset_details(data, db)
+
+    send_reset_email.delay(email, token)
+    return {"message": "If the email exists, a reset link has been sent"}
+
+
+@router.patch("/reset-password")
+async def reset_password(
+    token: str,
+    password: Annotated[str, Form],
+    db: Annotated[AsyncSession, Depends(get_db)],
+):
+    if not token:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST, detail="Please provide reset token"
+        )
+    result = await db.execute(
+        select(models.PasswordReset).where(models.PasswordReset.token == token)
+    )
+    db_token = result.scalars().first()
+    if not db_token:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Token not found")
+    now = datetime.now(UTC)
+    expiry = db_token.expires_at
+    if expiry <= now:
+        await db.execute(
+            delete(models.PasswordReset).where(models.PasswordReset.token == token)
+        )
+        await db.commit()
+
+        return {"message": "Token expired"}
+    await update_password(db_token.user_id, password, db)
+    await db.execute(
+        delete(models.PasswordReset).where(models.PasswordReset.token == token)
+    )
+    await db.commit()
+    await db.refresh(db_token)
+    return {"message": "Password reset successful, login with new password"}
